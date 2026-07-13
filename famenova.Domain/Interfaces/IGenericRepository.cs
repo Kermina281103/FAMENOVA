f277003c3bcd8e523716aca9045fd0801ff95194
KerminaMaged <carmina.maged.matta@gmail.com>
2026-07-14 02:20:34 +0300

    GenericRepositorey

--- famenova.Domain/Interfaces/IGenericRepository.cs
+++ famenova.Domain/Interfaces/IGenericRepository.cs
@@ -6,5 +6,12 @@ using System.Threading.Tasks;
 
 namespace famenova.Domain.Interfaces
 {
-   
+   public interface IGenericRepository<TEntity> where TEntity:class
+    {
+        Task AddAsync(TEntity entity);
+        void RemoveAsync(TEntity entity);
+        void UpdateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+    }
 }
---
+++ famenova.Infrastructure/Persistence/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using famenova.Domain.Interfaces;
+using famenova.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace famenova.Infrastructure.Persistence
+{
+    public class GenericRepository<TEntity>(AppDbContext _context) : IGenericRepository<TEntity> where TEntity : class
+    {
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public  void  RemoveAsync(TEntity entity)
+        {
+             _context.Set<TEntity>().Remove(entity);
+        }
+
+        public void UpdateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+    }
+}
